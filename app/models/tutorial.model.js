3d9b6e4d98777997aca318815e9f2b2086127182
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-13 04:12:10 -0300

    Update object model

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,13 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
   const Tutorial = sequelize.define("tutorial", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV1,
+      primaryKey: true
+    },
     title: {
       type: Sequelize.STRING
     },
     description: {
-      type: Sequelize.STRING
+      type: Sequelize.INTEGER
     },
     published: {
-      type: Sequelize.BOOLEAN
+      type: Sequelize.ENUM('income', 'expense')
+    },
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW
     }
   });
 
